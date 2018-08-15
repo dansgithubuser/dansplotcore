a9afe56c790a4d32a44b987c8e2a4ba9661eedce
Dan <dansonlinepresence@gmail.com>
2018-08-14 23:28:09 -0400

    bump version

--- package/setup.py
+++ package/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
 	name='dansplotcore',
-	version='1.0.2',
+	version='1.1.0',
 	description='plots powered by vertex buffer objects',
 	url='http://github.com/dansgithubuser/dansplotcore',
 	author='danspypiuser',
 	author_email='dansonlinepresence@gmail.com',
 	license='MIT',
 	packages=['dansplotcore'],
-	install_requires=['danssfmlpy'],
+	install_requires=['danssfmlpy==1.1.0'],
 	zip_safe=False,
 )
