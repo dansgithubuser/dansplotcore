c986b973ae34b32806d966b611ada634b68bc743
Dan <dansonlinepresence@gmail.com>
2018-08-16 22:15:52 -0400

    bump patch version, require newer version of danssfmlpy

--- package/setup.py
+++ package/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
 	name='dansplotcore',
-	version='1.1.1',
+	version='1.1.2',
 	description='plots powered by vertex buffer objects',
 	url='http://github.com/dansgithubuser/dansplotcore',
 	author='danspypiuser',
 	author_email='dansonlinepresence@gmail.com',
 	license='MIT',
 	packages=['dansplotcore'],
-	install_requires=['danssfmlpy==1.1.0'],
+	install_requires=['danssfmlpy==1.2.0'],
 	zip_safe=False,
 )
