18d381fc38af27c6d54fe58cb3bc1e9ffdcda86d
Dan <dansonlinepresence@gmail.com>
2020-01-16 18:58:56 -0500

    add long_description to package

--- package/setup.py
+++ package/setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
+with open('README.md') as f: readme = f.read()
+
 setup(
-	name='dansplotcore',
-	version='1.1.2',
-	description='speedy interactive plots',
-	url='http://github.com/dansgithubuser/dansplotcore',
-	author='danspypiuser',
-	author_email='dansonlinepresence@gmail.com',
-	license='MIT',
-	packages=['dansplotcore'],
-	install_requires=['danssfmlpy==1.2.0'],
-	zip_safe=False,
+    name='dansplotcore',
+    version='1.1.2',
+    description='speedy interactive plots',
+    long_description=readme,
+    url='http://github.com/dansgithubuser/dansplotcore',
+    author='danspypiuser',
+    author_email='dansonlinepresence@gmail.com',
+    license='MIT',
+    packages=['dansplotcore'],
+    install_requires=['danssfmlpy==1.2.0'],
+    zip_safe=False,
 )
