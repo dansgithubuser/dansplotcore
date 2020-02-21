fdcc80bfd960f16f67e8a3256e32fb9e3c31cd58
Dan <dansonlinepresence@gmail.com>
2020-02-21 16:55:58 -0500

    1.4.1 fix danssfml version requirement

--- package/setup.py
+++ package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dansplotcore',
-    version='1.4.0',
+    version='1.4.1',
     description='low-latency interactive plots',
     long_description="Dan's Plot Core is a minimal library that adds plots with low-latency interactivity to Python.",
     url='http://github.com/dansgithubuser/dansplotcore',
@@ -10,6 +10,6 @@ setup(
     author_email='dansonlinepresence@gmail.com',
     license='MIT',
     packages=['dansplotcore'],
-    install_requires=['danssfmlpy==1.5.0'],
+    install_requires=['danssfmlpy==1.6.0'],
     zip_safe=False,
 )
