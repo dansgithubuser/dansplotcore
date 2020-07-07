e973b78ac024fc13f7a9af4f68e1db403aafca1b
Dan <dansonlinepresence@gmail.com>
2020-07-06 22:16:49 -0400

    pull out rendering code

--- dansplotcore/plot.py
+++ dansplotcore/plot.py
@@ -1,16 +1,4 @@
 import math
-import os
-import re
-import sys
-
-DIR = os.path.dirname(os.path.realpath(__file__))
-
-wrapper_path = os.path.join(DIR, 'danssfml', 'wrapper')
-if os.path.exists(wrapper_path):
-    sys.path.append(wrapper_path)
-    import media
-else:
-    from danssfmlpy import media
 
 class View:
     def __init__(self, x=None, y=None, w=None, h=None):
@@ -69,157 +57,8 @@ class Plot:
         self._include(x, y)
 
     def show(self, w=640, h=480):
-        media.init(w, h, title=self.title)
-        media.custom_resize(True)
-        dragging = False
-        mouse = [0, 0]
-        if self.x_min == self.x_max:
-            self.x_min -= 1
-            self.x_max += 1
-        if self.y_min == self.y_max:
-            self.y_min -= 1
-            self.y_max += 1
-        dx = self.x_max - self.x_min
-        dy = self.y_max - self.y_min
-        self.x_min -= dx / 16
-        self.y_min -= dy / 16
-        self.x_max += dx / 16
-        self.y_max += dy / 16
-        view = View()
-        def reset():
-            view.x = self.x_min
-            view.y = self.y_min
-            view.w = self.x_max-self.x_min
-            view.h = self.y_max-self.y_min
-            media.view_set(*view.tuple())
-            self.is_reset = True
-        def move(view, dx, dy):
-            view.x -= dx*view.w/media.width()
-            view.y -= dy*view.h/media.height()
-            media.view_set(*view.tuple())
-            self.is_reset = False
-        def zoom(view, zx, zy, x, y):
-            # change view so (x, y) stays put and (w, h) multiplies by (zx, zy)
-            new_view_w = view.w*zx
-            new_view_h = view.h*zy
-            view.x += x/media.width () * (view.w - new_view_w)
-            view.y += y/media.height() * (view.h - new_view_h)
-            view.w = new_view_w
-            view.h = new_view_h
-            media.view_set(*view.tuple())
-            self.is_reset = False
-        reset()
-        self._construct()
-        while True:
-            # handle events
-            while True:
-                event = media.poll_event()
-                if not event: break
-                # quit
-                if event == 'q':
-                    media.close()
-                    return
-                # resize
-                m = re.match(r'rw(\d+)h(\d+)', event)
-                if m:
-                    w, h = (int(i) for i in m.groups())
-                    zoom(view, w/media.width(), h/media.height(), w/2, h/2)
-                    continue
-                # left mouse button
-                if event[0] == 'b':
-                    dragging = {'<': True, '>': False}[event[1]]
-                    if dragging:
-                        m = re.match(r'b<0x(\d+)y(\d+)', event)
-                        drag_prev = (int(i) for i in m.groups())
-                    continue
-                # mouse move
-                m = re.match(r'x(\d+)y(\d+)', event)
-                if m:
-                    mouse = [int(i) for i in m.groups()]
-                    if dragging:
-                        xi, yi = drag_prev
-                        dx, dy = mouse[0]-xi, mouse[1]-yi
-                        move(view, dx, dy)
-                        drag_prev = mouse
-                    continue
-                # mouse wheel
-                if event.startswith('w'):
-                    delta = int(event[1:])
-                    z = 1.25 if delta > 0 else 0.8
-                    zoom(view, z, z, mouse[0], mouse[1])
-                # keyboard
-                m = re.match('<(.+)', event)
-                if m:
-                    key = m.group(1)
-                    moves = {
-                        'Left' : ( 10,   0),
-                        'Right': (-10,   0),
-                        'Up'   : (  0,  10),
-                        'Down' : (  0, -10),
-                    }
-                    if key in moves:
-                        move(view, *moves[key])
-                        continue
-                    zooms = {
-                        'a': (1.25, 1),
-                        'd': (0.80, 1),
-                        'w': (1, 1.25),
-                        's': (1, 0.80),
-                    }
-                    if key in zooms:
-                        zoom(view, *zooms[key], media.width()/2, media.height()/2)
-                        continue
-                    if key == 'Space':
-                        if not self.is_reset:
-                            reset()
-                        else:
-                            view.x = 0.0
-                            view.y = self.y_min
-                            view.w = self.x_max
-                            view.h = -self.y_min
-                            media.view_set(*view.tuple())
-                            self.is_reset = False
-                    elif key == 'Return': media.capture_start()
-            # draw
-            media.clear(color=(0, 0, 0))
-            for i in self.vertex_buffers: i.draw()
-            margin_x = 2.0 / media.width()  * view.w
-            margin_y = 2.0 / media.height() * view.h
-            aspect = media.height() / media.width() * view.w / view.h
-            text_h = 10.0/media.height()*view.h
-            ## draw texts
-            for (s, x, y, r, g, b, a) in self.texts:
-                media.vector_text(s, x=x, y=y-text_h/4, h=text_h, aspect=aspect, r=r, g=g, b=b, a=a)
-                media.line(x=x, y=y, w=text_h, h=0, r=r, g=g, b=b, a=a)
-            if not self.hide_axes:
-                ## draw x axis
-                increment = 10 ** int(math.log10(view.w))
-                if view.w / increment < 2:
-                    increment /= 5
-                elif view.w / increment < 5:
-                    increment /= 2
-                i = view.x // increment * increment + increment
-                while i < view.x + view.w:
-                    s = '{:.5}'.format(i)
-                    if view.x + view.w - i > increment:
-                        media.vector_text(s, x=i+margin_x, y=view.y+view.h-margin_y, h=text_h, aspect=aspect)
-                    media.line(xi=i, xf=i, y=view.y+view.h, h=-12.0/media.height()*view.h)
-                    i += increment
-                ## draw y axis
-                increment = 10 ** int(math.log10(view.h))
-                if view.h / increment < 2:
-                    increment /= 5
-                elif view.h / increment < 5:
-                    increment /= 2
-                i = (view.y + text_h + 2*margin_y) // increment * increment + increment
-                while i < view.y + view.h - (text_h + 2*margin_y):
-                    s = '{:.5}'.format(-i)
-                    media.vector_text(s, x=view.x+margin_x, y=i-margin_y, h=text_h, aspect=aspect)
-                    media.line(x=view.x, w=12.0/media.width()*view.w, yi=i, yf=i)
-                    i += increment
-            ## display
-            media.display()
-            media.capture_finish(self.title+'.png')
+        from .show import show
+        show(self, w, h)
 
     def plot_list(self, l):
         for i, v in enumerate(l):
@@ -278,20 +117,6 @@ class Plot:
             raise Exception('unknown plot type for argument types {}'.format([type(i) for i in args]))
         plot_func(*args, **kwargs)
         return self
-
-    def _construct(self):
-        # points
-        points = media.VertexBuffer(len(self.points))
-        for i, point in enumerate(self.points):
-            points.update(i, *point)
-        # lines
-        lines = media.VertexBuffer(2*len(self.lines))
-        lines.set_type('lines')
-        for i, (xi, yi, xf, yf, r, g, b, a) in enumerate(self.lines):
-            lines.update(2*i+0, xi, yi, r, g, b, a)
-            lines.update(2*i+1, xf, yf, r, g, b, a)
-        # order
-        self.vertex_buffers = [lines, points]
 
     def _include(self, x, y):
         self.x_min = min(x, self.x_min)
